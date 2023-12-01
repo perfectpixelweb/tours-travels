0a1ed8f7bae29c4a267e5bfa52b972de1901935c
Mofazzal-Hossain <mofazzalhossain8@gmail.com>
2023-12-01 22:07:09 +0600

    2nd time commit

--- js/main.js
+++ js/main.js
@@ -12,3 +12,16 @@ document.querySelectorAll(".nav-link").forEach( n => n.addEventListener("click",
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+// ================Sticky Menu =====
+
+var navbar = document.getElementById('navbar');
+
+window.onscroll = function (){
+    if(window.pageYOffset >= hero.offsetTop){
+        navbar.classList.add("sticky");
+    }
+    else{
+        navbar.classList.remove("sticky");
+    }
+}
